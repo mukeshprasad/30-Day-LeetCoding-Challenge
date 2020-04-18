85b4ec499207c94194dc971cdb57029e6079273e
mukeshprasad <mukesh970378@gmail.com>
2020-04-18 10:43:01 +0530

    Create num_of_islands.py

--- Week-3/num_of_islands.py
+++ Week-3/num_of_islands.py
@@ -26,16 +26,16 @@
   Output: 3
 '''
 
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if len(grid) < 1:
             return 0
 
         def dfs(r, c):
-            if r<0 or c<0 or r>rows-1 or c>cols-1 or grid[r][c]=='0':
+            if r < 0 or c < 0 or r > rows-1 or c > cols-1 or grid[r][c] == '0':
                 return
             grid[r][c] = '0'
-
 
             dfs(r-1, c)
             dfs(r+1, c)
