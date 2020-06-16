fd7472d5fa0bafdcdccef93e44e3d0a7f030368c
mukeshprasad <44356982+mukeshprasad@users.noreply.github.com>
2020-06-16 14:43:36 +0530

    Validate IP Address - Medium - Must - Regex -  4 Solutions Added
    Have a Go!

---
+++ June-LeetCoding-Challenge/Week-3/valid_ip_address.py
@@ -0,0 +1,123 @@
+'''
+# Validate IP Address
+
+Solution
+Write a function to check whether an input string is a valid IPv4 address or IPv6 address or neither.
+
+IPv4 addresses are canonically represented in dot-decimal notation, which consists of four decimal numbers,
+each ranging from 0 to 255, separated by dots ("."), e.g.,172.16.254.1;
+
+Besides, leading zeros in the IPv4 is invalid. For example, the address 172.16.254.01 is invalid.
+
+IPv6 addresses are represented as eight groups of four hexadecimal digits, each group representing 16 bits.
+The groups are separated by colons (":"). For example, the address 2001:0db8:85a3:0000:0000:8a2e:0370:7334 is a valid one.
+Also, we could omit some leading zeros among four hexadecimal digits and some low-case characters in the address to upper-case ones,
+so 2001:db8:85a3:0:0:8A2E:0370:7334 is also a valid IPv6 address(Omit leading zeros and using upper cases).
+
+However, we don't replace a consecutive group of zero value with a single empty group using two consecutive colons (::) to pursue
+simplicity. For example, 2001:0db8:85a3::8A2E:0370:7334 is an invalid IPv6 address.
+
+Besides, extra leading zeros in the IPv6 is also invalid. For example, the address 02001:0db8:85a3:0000:0000:8a2e:0370:7334 is invalid.
+
+Note: You may assume there is no extra space or special characters in the input string.
+
+Example 1:
+Input: "172.16.254.1"
+
+Output: "IPv4"
+
+Explanation: This is a valid IPv4 address, return "IPv4".
+Example 2:
+Input: "2001:0db8:85a3:0:0:8A2E:0370:7334"
+
+Output: "IPv6"
+
+Explanation: This is a valid IPv6 address, return "IPv6".
+Example 3:
+Input: "256.256.256.256"
+
+Output: "Neither"
+
+Explanation: This is neither a IPv4 address nor a IPv6 address.
+'''
+
+
+# SOLUTION - 1: 
+class Solution:
+    # 354372986 98%
+    def validIPAddress(self, IP: str) -> str:
+        def isIPv4(s):
+            try: return str(int(s)) == s and 0 <= int(s) <= 255
+            except: return False
+            
+        def isIPv6(s):
+            if len(s) > 4: return False
+            try: return int(s, 16) >= 0 and s[0] != '-'
+            except: return False
+        
+        if IP.count('.') == 3 and all(isIPv4(s) for s in IP.split('.')):
+            return 'IPv4'
+        if IP.count(':') == 7 and all(isIPv6(s) for s in IP.split(':')):
+            return 'IPv6'
+        return 'Neither
+        
+# SOLUTION - 2: 
+
+from ipaddress import ip_address, IPv6Address
+class Solution:
+    def validIPAddress(self, IP: str) -> str:
+        try:
+            return "IPv6" if type(ip_address(IP)) is IPv6Address else "IPv4"
+        except ValueError:
+            return "Neither"
+            
+# SOLUTION - 3: Regex - Time -> O(1) <- Space
+
+import re
+class Solution:
+    chunk_IPv4 = r'([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
+    patten_IPv4 = re.compile(r'^(' + chunk_IPv4 + r'\.){3}' + chunk_IPv4 + r'$')
+    
+    chunk_IPv6 = r'([0-9a-fA-F]{1,4})'
+    patten_IPv6 = re.compile(r'^(' + chunk_IPv6 + r'\:){7}' + chunk_IPv6 + r'$')
+
+    def validIPAddress(self, IP: str) -> str:        
+        if self.patten_IPv4.match(IP):
+            return "IPv4"
+        return "IPv6" if self.patten_IPv6.match(IP) else "Neither" 
+        
+# SOLUTION - 4: O(N) Time and O(1) Space 
+
+class Solution:
+    def validate_IPv4(self, IP: str) -> str:
+        nums = IP.split('.')
+        for x in nums:
+            # Validate integer in range (0, 255):
+            # 1. length of chunk is between 1 and 3
+            if len(x) == 0 or len(x) > 3:
+                return "Neither"
+            # 2. no extra leading zeros
+            # 3. only digits are allowed
+            # 4. less than 255
+            if x[0] == '0' and len(x) != 1 or not x.isdigit() or int(x) > 255:
+                return "Neither"
+        return "IPv4"
+    
+    def validate_IPv6(self, IP: str) -> str:
+        nums = IP.split(':')
+        hexdigits = '0123456789abcdefABCDEF'
+        for x in nums:
+            # Validate hexadecimal in range (0, 2**16):
+            # 1. at least one and not more than 4 hexdigits in one chunk
+            # 2. only hexdigits are allowed: 0-9, a-f, A-F
+            if len(x) == 0 or len(x) > 4 or not all(c in hexdigits for c in x):
+                return "Neither"
+        return "IPv6"
+        
+    def validIPAddress(self, IP: str) -> str:
+        if IP.count('.') == 3:
+            return self.validate_IPv4(IP)
+        elif IP.count(':') == 7:
+            return self.validate_IPv6(IP)
+        else:
+            return "Neither"
