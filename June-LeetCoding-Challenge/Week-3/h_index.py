d3651543e1241a119089cca171cf048a073c1c6a
mukeshprasad <44356982+mukeshprasad@users.noreply.github.com>
2020-06-18 19:20:28 +0530

    H-Index II - Medium - O(logN) - Binary Search - Imp

---
+++ June-LeetCoding-Challenge/Week-3/h_index.py
@@ -0,0 +1,45 @@
+'''
+# H-Index II
+
+# Given an array of citations sorted in ascending order (each citation is a non-negative integer) of a researcher,
+  write a function to compute the researcher's h-index.
+
+# According to the definition of h-index on Wikipedia:
+  "A scientist has index h if h of his/her N papers have at least h citations each,
+  and the other N − h papers have no more than h citations each."
+
+# Example:
+
+  Input: citations = [0,1,3,5,6]
+  Output: 3 
+  
+  Explanation: [0,1,3,5,6] means the researcher has 5 papers in total and each of them had 
+             received 0, 1, 3, 5, 6 citations respectively. 
+             Since the researcher has 3 papers with at least 3 citations each and the remaining 
+             two with no more than 3 citations each, her h-index is 3.
+# Note:
+  If there are several possible values for h, the maximum one is taken as the h-index.
+
+# Follow up:
+
+  This is a follow up problem to H-Index, where citations is now guaranteed to be sorted in ascending order.
+  Could you solve it in logarithmic time complexity?
+'''
+
+# SOLUTION: Time - O(logN), Space - O(1) - Binary Search
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        # 355337603 99.2%
+        low, high = 0, len(citations) - 1
+        while low <= high:
+            mid = low + (high-low)//2
+            
+            if citations[mid] == len(citations) - mid:
+                return citations[mid]
+            elif citations[mid] > (len(citations) - mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+                
+        return len(citations) - low
