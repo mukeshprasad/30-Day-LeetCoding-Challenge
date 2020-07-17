89fdbed2f0fb041bec4aeb544723625ff0da53da
mukeshprasad <44356982+mukeshprasad@users.noreply.github.com>
2020-07-17 18:06:34 +0530

    Medium - Top K Frequent Elements - Quick Select - Must!!
    Many Solutions Added

---
+++ July-LeetCoding-Challenge/Week-3/top_K_frequent.py
@@ -0,0 +1,81 @@
+# Top K Frequent Elements
+
+# SOLUTION: Quick Select - Hoare's Selection Algorithm - Time complexity: O(N) in the average case, O(N^2) in the worst case. 
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        unique = list(count.keys())
+        
+        def partition(left, right, pivot_index) -> int:
+            pivot_frequency = count[unique[pivot_index]]
+            # 1. move pivot to end
+            unique[pivot_index], unique[right] = unique[right], unique[pivot_index]  
+            
+            # 2. move all less frequent elements to the left
+            store_index = left
+            for i in range(left, right):
+                if count[unique[i]] < pivot_frequency:
+                    unique[store_index], unique[i] = unique[i], unique[store_index]
+                    store_index += 1
+
+            # 3. move pivot to its final place
+            unique[right], unique[store_index] = unique[store_index], unique[right]  
+            
+            return store_index
+        
+        def quickselect(left, right, k_smallest) -> None:
+            """
+            Sort a list within left..right till kth less frequent element
+            takes its place. 
+            """
+            # base case: the list contains only one element
+            if left == right: 
+                return
+            
+            # select a random pivot_index
+            pivot_index = random.randint(left, right)     
+                            
+            # find the pivot position in a sorted list   
+            pivot_index = partition(left, right, pivot_index)
+            
+            # if the pivot is in its final sorted position
+            if k_smallest == pivot_index:
+                 return 
+            # go left
+            elif k_smallest < pivot_index:
+                quickselect(left, pivot_index - 1, k_smallest)
+            # go right
+            else:
+                quickselect(pivot_index + 1, right, k_smallest)
+                
+        n = len(unique) 
+        # kth top frequent element is (n - k)th less frequent.
+        # Do a partial sort: from less frequent to the most frequent, till
+        # (n - k)th less frequent element takes its place (n - k) in a sorted array. 
+        # All element on the left are less frequent.
+        # All the elements on the right are more frequent.  
+        quickselect(0, n - 1, n - k)
+        # Return top k frequent elements
+        return unique[n - k:] 
+ 
+ # MORE
+        # 1.
+        return [key[0] for key in Counter(nums).most_common(k)]
+        
+        #2. O(Nlogk)
+        if k == len(nums):
+            return nums
+        f = Counter(nums)
+        return heapq.nlargest(k, f.keys(), key=f.get)
+    
+        #3. O(N)
+        bucket = [[] for _ in nums]
+        for num, freq in Counter(nums).items():
+            bucket[-freq].append(num)
+        return list(itertools.chain(*bucket))[:k]
+    
+        #4. Isn't bad
+        keys = list(Counter(nums).keys())
+        count = Counter(nums)
+        keys.sort(key=count.get)
+        return keys[-k:]
