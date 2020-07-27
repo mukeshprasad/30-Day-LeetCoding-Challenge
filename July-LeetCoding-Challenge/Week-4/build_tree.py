87dafdf7b9678a2db631f3c1e5e43b18c2d60692
mukeshprasad <44356982+mukeshprasad@users.noreply.github.com>
2020-07-27 19:51:20 +0530

    Medium - Binary Tree from Inorder and Postorder Traversal - O(N) - IMP

--- July-LeetCoding-Challenge/Week-4/build_tree.py
+++ July-LeetCoding-Challenge/Week-4/build_tree.py
@@ -21,6 +21,13 @@
 
 '''
 # SOLUTION:
+# Definition of a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#           self.val = val
+#           self.left = left
+#           self.right = right
+          
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
         # 0. O(N^2) Time
