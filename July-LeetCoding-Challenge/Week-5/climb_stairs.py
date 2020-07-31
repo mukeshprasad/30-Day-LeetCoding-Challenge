d437cdb50c4b54c679b658cabd351748a27e3635
mukeshprasad <44356982+mukeshprasad@users.noreply.github.com>
2020-07-31 18:45:18 +0530

    Easy - Climbing Stairs - O(N) and O(logn) - Must Check!

---
+++ July-LeetCoding-Challenge/Week-5/climb_stairs.py
@@ -0,0 +1,47 @@
+ '''
+# Climbing Stairs
+
+# You are climbing a stair case. It takes n steps to reach to the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+# Example 1:
+
+  Input: 2
+  Output: 2
+  Explanation: There are two ways to climb to the top.
+  1. 1 step + 1 step
+  2. 2 steps
+
+# Example 2:
+
+  Input: 3
+  Output: 3
+  Explanation: There are three ways to climb to the top.
+  1. 1 step + 1 step + 1 step
+  2. 1 step + 2 steps
+  3. 2 steps + 1 step 
+
+# Constraints:
+    1 <= n <= 45
+'''
+ 
+  # SOLUTION:
+  class Solution:
+      def climbStairs(self, n: int) -> int:
+          # 1. O(N) Time and O(1) - Space
+          a = b = 1
+          for i in range(n):
+              a, b = b, a+b
+          return a
+         
+          # 2.
+          a, b = 0, 1
+          for i in range(n):
+              a, b = b, a+b
+          return b
+          
+          # 3. O(logn) Time and O(1) Space
+          sqrt5 = math.sqrt(5)
+          fibn = pow((1+sqrt5)/2,n+1) - pow((1-sqrt5)/2,n+1)
+          return int(fibn // sqrt5)
