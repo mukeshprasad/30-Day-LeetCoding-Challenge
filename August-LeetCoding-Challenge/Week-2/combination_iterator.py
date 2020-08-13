1ea99daf9543b10732459d31a3ab4b282341910d
mukeshprasad <44356982+mukeshprasad@users.noreply.github.com>
2020-08-13 20:47:56 +0530

    Medium - Iterator for Combination - Must One!

---
+++ August-LeetCoding-Challenge/Week-2/combination_iterator.py
@@ -0,0 +1,48 @@
+'''
+# Iterator for Combination
+
+# Design an Iterator class, which has:
+
+# A constructor that takes a string characters of sorted distinct lowercase English letters and a number combinationLength as arguments.
+# A function next() that returns the next combination of length combinationLength in lexicographical order.
+# A function hasNext() that returns True if and only if there exists a next combination.
+ 
+# Example:
+  CombinationIterator iterator = new CombinationIterator("abc", 2); // creates the iterator.
+
+  iterator.next(); // returns "ab"
+  iterator.hasNext(); // returns true
+  iterator.next(); // returns "ac"
+  iterator.hasNext(); // returns true
+  iterator.next(); // returns "bc"
+  iterator.hasNext(); // returns false
+ 
+# Constraints:
+  * 1 <= combinationLength <= characters.length <= 15
+  * There will be at most 10^4 function calls per test.
+  * It's guaranteed that all calls of the function next are valid.
+'''
+# SOLUTION:
+# 1.
+class CombinationIterator:
+    def __int__(self, characters: str, combinationLength: int):
+        combs = list(map(''.join, itertools.combinations(characters, combinationLength)))[::-1]
+        self.next = combs.pop
+        self.hasNext = lambda: bool(combs)
+        
+    def next(self) -> str:
+        return
+        
+    def hasNext(self) -> bool:
+        return
+        
+# 2.
+class CombinationIterator:
+    def __int__(self, characters: str, combinationLength: int):
+        self.combs = list(map(''.join, itertools.combinations(characters, combinationLength)))[::-1]
+       
+    def next(self) -> str:
+        return self.combs.pop()
+        
+    def hasNext(self) -> bool:
+        return bool(combs)
